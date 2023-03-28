6dbf46b3e35f842cae958e4528d0c8afa5748d99
mmartinamezcua <mmartinamezcua@hiberus.com>
2023-03-28 17:45:18 +0200

    primer commit

---
+++ src/components/pure/greeting.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Greeting extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+
+Greeting.propTypes = {
+
+};
+
+
+export default Greeting;
